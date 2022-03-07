0ff87950257aababbb19911e4caf8026ebdc868c
Trần Khánh Linh <trankhanhlinhvcm@gmail.com>
2022-03-07 15:44:23 +0700

    update 1

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log('hello')
+// const http = require('http'); 
+
+// const sever = http.createServer((req, res) => {
+
+//     const url = req.url;
+//     if(url === '/product'){
+//         console.log('Product')
+//     } else {
+//         console.log(' Home')
+//     }
+// });
+
+// const PORT = 3001;
+// sever.listen(PORT, () => {
+//     console.log(`Sever running port ${PORT}`);
+// });
+
+//
+const http = require('http'); 
+
+const sever = http.createServer((req, res) => {
+
+    const url = req.url;
+    if(url === '/product'){
+       res.setHeader('content-Type','text/html');
+       res.write("<html>");
+       res.write("<body>");
+            res.write("<h1> Home page</h1>");
+       res.write("<body>");
+       res.write("<html>");
+    } else {
+       res.write("<html>");
+       res.write("<body>");
+            res.write("<h1> Home page new</h1>");
+       res.write("<body>");
+       res.write("<html>");
+       res.end();
+    }
+});
+
+const PORT = 3001;
+sever.listen(PORT, () => {
+    console.log(`Sever running port ${PORT}`);
+});
